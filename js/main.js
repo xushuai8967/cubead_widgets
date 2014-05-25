cd46a51dfaf170237e39a6d68342a93b157cbb92
ShuaiXu <shuaixu@aliyun.com>
2014-05-25 13:38:10 +0800

    add some properties

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 require.config({
 	paths:{
-		jquery : 'jquery-1.11.1.min'
+		jquery : 'jquery-1.11.1.min',
+		jqueryUI : 'jquery-ui'
 	}
 });
 
 require(['jquery','window'],function($, w){
 	$('#a').click(function(){
 		var window = new w.Window()
-		window.alert("hello world!");
+		window.alert( {
+			width : 500,
+			height : 200,
+			y: 100,
+			content : "hello world!",
+			alertBtnText : "没问题",
+			hasCloseBtn : true,
+			dragHandler : '.window_header',
+			skinClassName : "window_skin_orange",
+			handler4CloseBtn : function(){
+				alert("closeBtn");
+			},
+			handler4AlertBtn : function(){
+				alert("alertBtn");
+			}
+		});
 	});
 });
--- js/window.js
+++ js/window.js
@@ -1,14 +1,89 @@
-define(['jquery'], function($){
-	function Window(){}
+define(['jquery','jqueryUI'], function($, $UI){
+	function Window(){
+		this.config = {
+			//宽
+			width : 400,
+			//高
+			height : 200,
+			//弹窗内容
+			content : "",
+			//标题
+			title : "系统消息",
+			//弹窗中的按钮文字内容
+			alertBtnText : "确定",
+			//是否使用模态
+			hasMask : true,
+			//定制皮肤名称
+			skinClassName : null,
+			//是否需要右上角关闭按钮
+			hasCloseBtn : false,
+			//是否支持拖拽
+			isDraggable : true,
+			//拖拽绑定事件
+			dragHandler : null,
+			//点击弹窗中的按钮后执行的函数
+			handler4AlertBtn : null,
+			//点击右上角关闭按钮后执行的函数
+			handler4CloseBtn : null
+		}
+	}
 
 	Window.prototype = {
-		alert:function(content){
-			var parent = $("<div class='window_alertParent'></div>"),
-				mask = $("<div class='window_alert'></div>");
-			
-			$('body').append(parent);
-			parent.append(mask);
-			mask.html(content);
+		alert:function(config){
+			var cfg = $.extend(this.config, config);
+			var box = $(
+				"<div class='window_alert'>" + 
+					"<div class='window_header'>" + cfg.title + "</div>" +
+					"<div class='window_body'>" + cfg.content + "</div>" +
+					"<div class='window_footer'><input type='button' class='window_alertBtn' value='" + cfg.alertBtnText + "'/> </div>" +
+				"</div>"
+				);
+
+			//注意mask要先于box添加到body上，否则会遮盖住弹窗
+			var mask = null;
+			if(cfg.hasMask){
+				mask = $("<div class='window_mask'></div>");
+				mask.appendTo('body');
+			}
+			//将弹窗添加到body
+			box.appendTo('body');
+			var btn = box.find('.window_footer .window_alertBtn');
+			//为弹窗中的按钮添加事件
+			btn.click(function(){
+				cfg.handler4AlertBtn && cfg.handler4AlertBtn();
+				box.remove();
+				mask && mask.remove();
+			});
+			//为弹窗设定位置和大小
+			box.css({	
+				width : cfg.width + "px",
+				height : cfg.height + "px",
+				left : (cfg.x || (window.innerWidth - cfg.width) / 2) + "px",
+				top : (cfg.y || (window.innerHeight - cfg.height) / 2) + "px"
+			});
+			//为弹窗右上角按钮添加事件
+			if(cfg.hasCloseBtn){
+				var closeBtn = $("<span class='window_closeBtn'>X<span>");
+				closeBtn.appendTo(box);
+				closeBtn.click(function(){
+					cfg.handler4CloseBtn && cfg.handler4CloseBtn();
+					box.remove();
+					mask && mask.remove();
+				});
+			}
+			//设定弹窗皮肤
+			if(cfg.skinClassName){
+				box.addClass(cfg.skinClassName);
+			}
+			//设定拖拽事件
+			if (cfg.isDraggable) {
+				if(cfg.dragHandler){
+					box.draggable({
+						handle : cfg.dragHandler
+					});
+				} else {
+				}
+			};
 		},
 		confirm:function(){},
 		prompt:function(){}
